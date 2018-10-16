bb7b9f9483fc81908f1ea02a032613b05da33729
shubham <shubhamtorvi18@gmail.com>
2018-10-16 21:47:52 +0530

    Final with changes

---
+++ SPAssessment/SPAssessment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using Microsoft.SharePoint.Client;
+namespace SPAssessment
+{
+    class Program
+    {
+        static bool LoginStatus;
+        static void Main(string[] args)
+        {
+            UserCredentials.Getdata();
+            string Url = "https://acuvatehyd.sharepoint.com/teams/shubhamtrial";
+            SiteData sitedata = new SiteData();
+            LoginStatus=sitedata.GetSiteData(Url);
+            if (LoginStatus == true)
+            {
+                sitedata.DownloadFile(Url);
+                sitedata.GetDocumentData(Url);
+                //sitedata.UploadData(Url, UserName, passwrd);
+            }
+            else
+            {
+                Main(args);
+            }
+            Console.ReadKey();
+        }
+    }
+}
